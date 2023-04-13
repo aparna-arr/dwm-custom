6540e72ceccfc75ce61af73f28e8a697ba20edfb
Aparna Rajpurkar <aparna.arr@gmail.com>
2023-04-13 01:02:33 -0400

    one patch applied, dmenu added

--- dwm.c
+++ dwm.c
@@ -441,11 +441,6 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + TEXTW(selmon->ltsymbol))
 			click = ClkLtSymbol;
-<<<<<<< ours
-		else if (ev->x > selmon->ww - (int)TEXTW(stext))
-			click = ClkStatusText;
-=======
->>>>>>> theirs
 		else
 			click = ClkStatusText;
 	} else if ((c = wintoclient(ev->window))) {
@@ -737,17 +732,7 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
-<<<<<<< ours
-	if ((w = m->ww - tw - x) > bh) {
-		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-			if (m->sel->isfloating)
-				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
-		} else {
-=======
 	if ((w = m->ww - sw - x) > bh) {
->>>>>>> theirs
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 	}
